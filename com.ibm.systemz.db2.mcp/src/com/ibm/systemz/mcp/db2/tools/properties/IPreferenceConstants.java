ac1d891fb6309684fdb830bb94764d90a1ab0e7a
Jeremy Flicker <jflicke@MacBookPro.attlocal.net>
2025-10-16 17:17:01 -0700

    cleanup for publishing

--- com.ibm.systemz.db2.mcp/src/com/ibm/systemz/mcp/db2/tools/properties/IPreferenceConstants.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.systemz.mcp.db2.tools.properties;
-
-import com.ibm.systemz.db2.Activator;
-
-public interface IPreferenceConstants {
-
-	public static final String P_ENABLEWRITES = Activator.kPluginID + "preference.enable.writes";
-	public static final String P_CONNECTIONID = Activator.kPluginID + "preference.connection.id";
-}
